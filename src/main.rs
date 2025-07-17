50c77c20e7ce109a705ae1d543dba6097c704735
= <pmosadeqzadeh@gmail.com>
2025-07-17 20:00:53 +0330

    chore: added write file

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,30 @@ fn read_errors(text: &str) -> Vec<String> {
     return error_lines;
 }
 
+fn write_to_file(file_name: &str, content: String) {
+    match fs::write(file_name, content) {
+        Ok(..) => {
+            println!(" successfully wrote to file");
+        }
+
+        Err(error_msg) => {
+            println!("error_msg: {:#?}", error_msg);
+        }
+    }
+}
+
 fn main() {
     let text = fs::read_to_string("002 logs.txt");
     let err_msg: Vec<String>;
+    let joined: String;
     match text {
         Ok(red_string) => {
             err_msg = read_errors(&red_string);
-            println!("{:#?}", err_msg);
+            joined = err_msg.join("\n");
+            write_to_file("error.txt", joined);
         }
-        Err(error_message) => println!("{}", error_message),
+        Err(error_message) => {
+            println!("{}", error_message);
+        }
     }
 }
