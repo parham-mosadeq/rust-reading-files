6409f1d5292401f87b070556e4015033b2fa110c
= <pmosadeqzadeh@gmail.com>
2025-07-17 18:22:48 +0330

    fix: reading it properly

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
 
 fn main() {
-    let text = fs::read_to_string("002 logs.txt").unwrap();
-    println!("{:#?}", text);
+    let text = fs::read_to_string("002 logs.txt");
+    match text {
+        Ok(..) => println!("{:#?}", text),
+        Err(error_message) => println!("{}", error_message),
+    }
 }
