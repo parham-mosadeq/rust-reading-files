e5d374501d93e4a08a8f72b55924f240c1781e69
= <pmosadeqzadeh@gmail.com>
2025-07-17 19:49:04 +0330

    fix: reading and passing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use std::fs;
 
+fn read_errors(text: &str) -> Vec<String> {
+    let splitted_text = text.split("\n");
+
+    let mut error_lines = vec![];
+
+    for line in splitted_text {
+        if line.contains("ERROR") {
+            error_lines.push(line.to_string());
+        }
+    }
+
+    return error_lines;
+}
+
 fn main() {
     let text = fs::read_to_string("002 logs.txt");
+    let err_msg: Vec<String>;
     match text {
-        Ok(red_string) => println!("{}", red_string),
+        Ok(red_string) => {
+            err_msg = read_errors(&red_string);
+            println!("{:#?}", err_msg);
+        }
         Err(error_message) => println!("{}", error_message),
     }
 }
