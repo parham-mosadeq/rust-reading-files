730274c2d82b5f0c1aabe2a25a985a2887650d88
= <pmosadeqzadeh@gmail.com>
2025-07-17 18:49:56 +0330

    fix: reading unwrapped

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main() {
     let text = fs::read_to_string("002 logs.txt");
     match text {
-        Ok(..) => println!("{:#?}", text),
+        Ok(red_string) => println!("{}", red_string),
         Err(error_message) => println!("{}", error_message),
     }
 }
